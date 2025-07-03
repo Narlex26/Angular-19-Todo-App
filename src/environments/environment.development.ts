7ba0a1b098d557c047b00b1ac8b50e4b6820ce43
Alexandre Boyer <alexandreboyer39@gmail.com>
2025-07-03 11:11:07 +0200

    Enhance environment configurations: add development and staging setups, update API keys, and document environment structure

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  weatherApiKey: '5ccad1686b1549b992d70017250602',
-  API_KEY:'36f1871726deab18e6c8261a15351637ac481b714f7f97bff660e13351c3ded9',
-  apiBaseUrl: 'http://212.83.131.87/api' 
-};
