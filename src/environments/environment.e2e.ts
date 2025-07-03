5c81126664c5d3ecec613b5d72f848d32ca96dd7
Alexandre Boyer <alexandreboyer39@gmail.com>
2025-07-03 09:33:18 +0200

    Add E2E configuration and environment settings for testing

---
+++ src/environments/environment.e2e.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  weatherApiKey: '5ccad1686b1549b992d70017250602',
+  API_KEY:'36f1871726deab18e6c8261a15351637ac481b714f7f97bff660e13351c3ded9',
+  apiBaseUrl: 'http://localhost:3000/api'
+};
+
