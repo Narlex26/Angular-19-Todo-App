5a352763df4f0cde59fbcf0733d986bfe0242f83
Alexandre Boyer <alexandreboyer39@gmail.com>
2025-07-07 11:38:37 +0200

    Update API proxy configuration and set API key in staging environment

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,5 +3,5 @@ export const environment = {
   staging: true,
   weatherApiKey: '5ccad1686b1549b992d70017250602',
   apiBaseUrl: 'http://212.83.131.87/api',
-  API_KEY:''
+  API_KEY:'36f1871726deab18e6c8261a15351637ac481b714f7f97bff660e13351c3ded9'
 };
